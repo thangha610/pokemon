00907e991594a2cbb2fff46a345f36b52a35251c
thangha610 <thangha@beeketing.net>
2019-09-11 14:36:30 +0700

    first commit

--- src/router.js
+++ src/router.js
@@ -1,27 +1,20 @@
 import Vue from "vue";
-import Router from "vue-router";
-import Home from "./views/Home.vue";
+import VueRouter from "vue-router";
+import Home from "./views/Home";
 
+// lazy load route
+const Favorites = () => import("./views/Favorites");
 
-Vue.use(Router);
+Vue.use(VueRouter);
 
-export default new Router({
+const routes = [
+  { path: "/favorites", component: Favorites, meta: { requiresAuth: true } },
+  { path: "*", component: Home, meta: { requiresAuth: true } }
+];
+
+const router = new VueRouter({
   mode: "history",
-  base: process.env.BASE_URL,
-  routes: [
-    {
-      path: "/",
-      name: "home",
-      component: Home
-    },
-    {
-      path: "/favorites",
-      name: "favorites",
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () =>
-        import(/* webpackChunkName: "about" */ "./views/Favorites.vue")
-    }
-  ]
+  routes // short for `routes: routes`
 });
+
+export default router;
